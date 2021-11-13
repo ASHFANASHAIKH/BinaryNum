45db10c0e979ceff67b2ce9130f96b5b30ec7831
Ashfana Ashfu <shaikhashfana9@gmail.com>
2021-11-13 20:21:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryNum
+{
+    class Program
+    {
+        static void bin(long n)
+        {
+            long i;
+            Console.Write("0");
+            for (i = 1 << 30; i > 0; i = i / 2)
+            {
+                if ((n & i) != 0)
+                {
+                    Console.Write("1");
+                }
+                else
+                {
+                    Console.Write("0");
+                }
+            }
+        }
+        
+        static public void Main()
+        {
+            bin(7);
+            Console.WriteLine();
+            bin(4);
+        }
+    }
+}
+
